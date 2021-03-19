b4691e3bb594f311846731b39ae429dd29fbc3c2
Nikita-as <80970900+Nikita-as@users.noreply.github.com>
2021-03-19 21:58:51 +0300

    init commit

---
+++ app/src/main/java/com/example/my/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.my
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun onClickTest(view: View) {
+        val edit = findViewById<EditText>(R.id.et1)
+        val textBlog = findViewById<TextView>(R.id.text2)
+        val value = edit.text.toString().toInt()
+        textBlog.text = when (value) {
+            in 0..99 -> "Ты начинающий ютубер"
+            in 100..99999 -> "Ты успешный ютубер"
+            in 10000..999999 -> "Ты популярный ютубер"
+            else -> "Ты самый лучший ютубер"
+        }
+
+    }
+}
